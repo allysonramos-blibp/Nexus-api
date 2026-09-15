ddaf4eef07f796f69cfa00cb054eb4353c3716a1
Allyson Ramos <allysonr510@gmail.com>
2026-09-15 16:02:12 +0000

    fix(mock-exam): adicionar import java.util.List em MockExamRepository

--- nexus-api/src/main/java/com/nexus/nexus_api/repository/MockExamRepository.java
+++ nexus-api/src/main/java/com/nexus/nexus_api/repository/MockExamRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface MockExamRepository extends JpaRepository<MockExam, Long> {
     List<MockExam> findByUserId(Long userId);
